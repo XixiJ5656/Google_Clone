e7d675393c111ea3790b3f96c93ed3596c08a3c2
sissy5656 <supersissy5656@gmail.com>
2020-10-10 22:53:34 -0500

    working

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,24 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
-
+import { Button } from "@material-ui/core";
 const Search = () => {
+  const search = (e) => {
+    e.preventDefautl();
+  };
+
   return (
     <div className="search">
       <div className="search-input">
         <SearchIcon className="search-icon" />
         <input />
         <MicIcon />
+      </div>
+      <div className="search-button">
+        <Button onClick={search} variant="outlined">
+          Google Search
+        </Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
   );
