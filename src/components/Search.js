aa9c568011a3cc74808fa6e869bbbade9dffa8b6
sissy5656 <supersissy5656@gmail.com>
2020-10-13 23:23:57 -0500

    working on search func

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
+
 const Search = () => {
+  const [input, setInput] = useState("");
+
   const search = (e) => {
-    e.preventDefautl();
+    e.preventDefault();
+    console.log("you hit search ", input);
   };
 
   return (
-    <div className="search">
+    <form className="search">
       <div className="search-input">
         <SearchIcon className="search-icon" />
-        <input />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon />
       </div>
       <div className="search-button">
-        <Button onClick={search} variant="outlined">
+        <Button type="submit" onClick={search} variant="outlined">
           Google Search
         </Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   );
 };
 
